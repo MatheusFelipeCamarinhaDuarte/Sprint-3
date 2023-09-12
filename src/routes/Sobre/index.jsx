cf8a80a953f1128fb08fa90a3fbfd4274d7cb1eb
BernardoliveiraFiap <RM551356@FIAP.COM.BR>
2023-09-12 09:03:54 -0300

    Sobre nos

--- src/routes/Sobre/index.jsx
+++ src/routes/Sobre/index.jsx
@@ -1,10 +1,32 @@
-export default function index() {
+export default function AboutUs() {
   return (
-<main>
-    <h1>
-        Sobre nós: 
-    </h1>
-    <p>nada ainda, volte mais tarde :)</p>
-</main>
-)
+    <main>
+      <h1>Sobre nós</h1>
+      
+      <p>
+        Somos uma equipe apaixonada por criar experiências incríveis para nossos clientes. Com anos de expertise e dedicação, estamos comprometidos em oferecer soluções inovadoras para suas necessidades.
+      </p>
+
+      <p>
+        Nossa missão é tornar sua vida mais fácil, fornecendo Seguros e serviços de alta qualidade que atendam às suas expectativas.
+      </p>
+
+      <p>
+      
+      551356 - Enzo Oliveira
+  
+      551416 - MACIRANDER SOUZA DE MIRANDA FILHO
+
+      98672 - Mirelly Ribeiro Azevedo
+
+      551401 - Ana Luiza Fontes Franco
+
+      551856 - Beatriz Fon Ehnert de Santi
+
+      552295 - MATHEUS FELIPE CAMARINHA DUARTE
+
+      </p>
+
+    </main>
+  );
 }
